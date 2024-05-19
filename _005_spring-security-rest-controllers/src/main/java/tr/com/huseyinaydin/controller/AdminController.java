d35d0600a7853f0bf7327fcc92b93e210bc1bd84
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-19 12:48:04 +0300

    Spring Boot Security RESTApi belirli adreslere yetkisi olan kişinin girebilmesi.

---
+++ _005_spring-security-rest-controllers/src/main/java/tr/com/huseyinaydin/controller/AdminController.java
@@ -0,0 +1,24 @@
+package tr.com.huseyinaydin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Boot Security
+*
+*/
+
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+
+    @GetMapping("index")
+    public String index(){
+        return "admin/index";
+    }
+}
---
+++ _005_spring-security-rest-controllers/src/main/java/tr/com/huseyinaydin/controller/PublicRestApiController.java
@@ -0,0 +1,29 @@
+package tr.com.huseyinaydin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Boot Security
+*
+*/
+
+@RestController
+@RequestMapping("api/public")
+public class PublicRestApiController {
+
+    @GetMapping("test1")
+    public String test1(){
+        return "API Test 1";
+    }
+
+    @GetMapping("test2")
+    public String test2(){
+        return "API Test 2";
+    }
+}
