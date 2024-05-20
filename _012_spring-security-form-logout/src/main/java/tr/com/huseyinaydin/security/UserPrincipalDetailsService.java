bfbfa64ae1b76ebf8c8a54b6809635b162006c7d
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-20 17:06:37 +0300

    Spring Boot Security H2 veritabanı form ile çıkış işlemi.

---
+++ _012_spring-security-form-logout/src/main/java/tr/com/huseyinaydin/security/UserPrincipalDetailsService.java
@@ -0,0 +1,37 @@
+package tr.com.huseyinaydin.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import tr.com.huseyinaydin.model.User;
+import tr.com.huseyinaydin.repository.UserRepository;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Boot Security
+*
+*/
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+   
+	@Autowired
+	private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(s);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+        return userPrincipal;
+    }
+}
