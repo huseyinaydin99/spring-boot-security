8b19b9778b51feacd6a6eccceafda6f9a875aab9
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-20 16:52:08 +0300

    Spring Boot Security özelleştirilmiş form ile kimlik doğrulama ve yetkilendirme.

---
+++ _011_spring-security-form-login/src/main/java/tr/com/huseyinaydin/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package tr.com.huseyinaydin.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import tr.com.huseyinaydin.model.User;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Boot Security
+*
+*/
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByUsername(String username);
+}
