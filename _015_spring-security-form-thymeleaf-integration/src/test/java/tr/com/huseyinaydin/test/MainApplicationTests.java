3e48d13472d8dbd049036c0985e60b77a9d1d4b9
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-21 17:52:40 +0300

    Spring Boot Security ve Thymeleaf entegrasyonu.

---
+++ _015_spring-security-form-thymeleaf-integration/src/test/java/tr/com/huseyinaydin/test/MainApplicationTests.java
@@ -0,0 +1,24 @@
+package tr.com.huseyinaydin.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Boot Security
+*
+*/
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
