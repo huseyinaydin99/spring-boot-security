440a1494ff5a90619c8998ef292c379fde2fa6c1
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-21 17:37:12 +0300

    Spring Boot Security formu ve form bilgilerini özelleştirmek.

---
+++ _014_spring-security-form-controls-customize/src/main/java/tr/com/huseyinaydin/controller/PublicRestApiController.java
@@ -0,0 +1,45 @@
+package tr.com.huseyinaydin.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tr.com.huseyinaydin.model.User;
+import tr.com.huseyinaydin.repository.UserRepository;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Boot Security
+*
+*/
+
+@RestController
+@RequestMapping("api/public")
+public class PublicRestApiController {
+	
+	private UserRepository userRepository;
+
+	public PublicRestApiController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@GetMapping("users")
+	public List<User> users() {
+		return this.userRepository.findAll();
+	}
+
+	@GetMapping("test1")
+	public String test1() {
+		return "API Test 1";
+	}
+
+	@GetMapping("test2")
+	public String test2() {
+		return "API Test 2";
+	}
+}
